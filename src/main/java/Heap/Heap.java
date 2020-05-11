fdb7552c79add0bd1fdc3cbfed5e2433d661b4db
xinglei.li <xinglei.li@zhaopin.com.cn>
2020-05-11 19:24:38 +0800

    hash & graph

---
+++ src/main/java/Heap/Heap.java
@@ -0,0 +1,76 @@
+package Heap;
+
+public class Heap {
+
+    private int[] a; // 数组，从下标 1 开始存储数据
+    private int n; // 堆可以存储的最大数据个数
+    private int count; // 堆中已存储的数据个数
+
+    public Heap(int capacity) {
+        a = new int[capacity + 1];
+        this.n = capacity;
+        this.count = 0;
+    }
+
+    /**
+     * 插入
+     * @param data
+     */
+    public void insert(int data) {
+        if (count >= n) {
+            return;
+        }
+
+        ++count;
+        a[count] = data;
+        int i = count;
+        while (i / 2 > 0 && a[i] > a[i / 2]) {
+            swap(a, i, i/2);
+            i = i/2;
+        }
+    }
+
+    /**
+     * 删除堆顶元素
+     */
+    public void removeMax() {
+        if (count == 0) {
+            return;
+        }
+
+        a[1] = a[count];
+        --count;
+
+        heapify(a, count, 1);
+    }
+
+    /**
+     * 自上而下堆化
+     * @param a
+     * @param n
+     * @param i
+     */
+    private void heapify(int[] a, int n, int i) {
+        while (true) {
+            int maxPos = i;
+            if (i * 2 <= n && a[i] < a[i * 2]) {
+                maxPos = i * 2;
+            }
+            if (i * 2 + 1 <= n && a[maxPos] < a[i * 2 + 1]) {
+                maxPos = i * 2 + 1;
+            }
+            if (maxPos == i) {
+                break;
+            }
+            swap(a, i, maxPos);
+            i = maxPos;
+        }
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
+
--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -1,5 +1,144 @@
 package graph;
 
-public class Graph {
+import java.util.LinkedList;
+import java.util.Queue;
 
+/**
+ * 无向图
+ */
+public class Graph {
+    private int v; // 顶点的个数
+    private LinkedList<Integer> adj[]; // 邻接表
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    /**
+     * 无向图一条边存两次
+     * @param s
+     * @param t
+     */
+    public void addEdge(int s, int t) {
+        adj[s].add(t);
+        adj[t].add(s);
+    }
+
+    /**
+     * 广度优先搜索
+     * @param s
+     * @param t
+     */
+    public void bfs(int s, int t) {
+        if (s == t) {
+            return;
+        }
+
+        // 已经被访问的顶点
+        boolean[] visited = new boolean[v];
+        visited[s] = true;
+
+        // 已经被访问、但相连的顶点还没有被访问的顶点
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+
+        // 搜索路径
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < adj[w].size(); ++i) {
+                int q = adj[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                    visited[q] = true;
+                    queue.add(q);
+                }
+            }
+        }
+    }
+
+    /**
+     * 递归打印 s->t 的路径
+     * @param prev
+     * @param s
+     * @param t
+     */
+    private void print(int[] prev, int s, int t) {
+        if (prev[t] != -1 && t != s) {
+            print(prev, s, prev[t]);
+        }
+        System.out.print(t + " ");
+    }
+
+
+    boolean found = false; // 全局变量或者类成员变量
+
+    /**
+     * 深度优先搜索
+     * @param s
+     * @param t
+     */
+    public void dfs(int s, int t) {
+        found = false;
+        boolean[] visited = new boolean[v];
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        recurDfs(s, t, visited, prev);
+        print(prev, s, t);
+    }
+
+    private void recurDfs(int w, int t, boolean[] visited, int[] prev) {
+        if (found == true) {
+            return;
+        }
+
+        visited[w] = true;
+
+        if (w == t) {
+            found = true;
+            return;
+        }
+
+        for (int i = 0; i < adj[w].size(); ++i) {
+            int q = adj[w].get(i);
+            if (!visited[q]) {
+                prev[q] = w;
+                recurDfs(q, t, visited, prev);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(8);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 3);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(3, 4);
+        graph.addEdge(4, 5);
+        graph.addEdge(4, 6);
+        graph.addEdge(5, 7);
+        graph.addEdge(6, 7);
+
+        graph.bfs(0, 6);
+
+        System.out.println();
+
+        graph.dfs(0, 6);
+    }
 }
