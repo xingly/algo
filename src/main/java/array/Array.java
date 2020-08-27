7ff4ecdcb7838a3ca44bf783c36821c4f6594eb5
xinglei.li <xinglei.li@zhaopin.com.cn>
2020-08-27 21:18:16 +0800

    动态数组

--- src/main/java/array/Array.java
+++ src/main/java/array/Array.java
@@ -1,5 +1,89 @@
 package array;
 
+import java.util.Arrays;
+
+/**
+ * 数组插入、删除、随机访问
+ */
 public class Array {
 
+    private int[] data;
+
+    private int n;
+
+    private int count;
+
+    public Array(int capacity) {
+        this.data = new int[capacity];
+        this.n = capacity;
+        this.count = 0;
+    }
+
+    /**
+     * 根据索引找到数组中元素并返回
+     *
+     * @param index
+     * @return
+     */
+    public int find(int index) {
+        if (index < 0 || index >= count) {
+            return -1;
+        }
+        return data[index];
+    }
+
+    /**
+     * 插入元素
+     *
+     * @param index
+     * @param value
+     * @return
+     */
+    public boolean insert(int index, int value) {
+        // 数组已满
+        if (count == n) {
+            return false;
+        }
+
+        // 位置不合法
+        if (index < 0 || index > count) {
+            return false;
+        }
+
+        for (int i = count; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = value;
+        count++;
+        return true;
+    }
+
+    /**
+     * 根据索引，删除数组中元素
+     *
+     * @param index
+     * @return
+     */
+    public boolean delete(int index) {
+        if (index < 0 || index >= count) {
+            return false;
+        }
+
+        for (int i = index; i <= count - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        count--;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Array array = new Array(10);
+        array.insert(0, 1);
+        array.insert(0, 2);
+        array.insert(0, 3);
+        array.insert(0, 4);
+        System.out.println(Arrays.toString(array.data));
+        System.out.println(array.count);
+        System.out.println(array.data.length);
+    }
 }
---
+++ src/main/java/array/GenericArray.java
@@ -0,0 +1,55 @@
+package array;
+
+public class GenericArray<T> {
+    private T[] data;
+    private int size;
+
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public GenericArray(int capacity) {
+        data = (T[]) new Object[capacity];
+        size = 0;
+    }
+
+    public GenericArray() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    // 数组容量
+    public int getCapacity() {
+        return data.length;
+    }
+
+    // 获取数组元素个数
+    public int count() {
+        return size;
+    }
+
+    // 判读数组是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 修改 index 位置元素
+    public void set(int index, T e) {
+        checkIndex(index);
+        data[index] = e;
+    }
+
+    // 获取 index 位置的元素
+    public T get(int index) {
+        checkIndex(index);
+        return data[index];
+    }
+
+
+
+
+    private boolean checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("index must >= 0 and < size.");
+        }
+        return true;
+    }
+
+}
