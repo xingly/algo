5d0184b50f505b9fcc4e04e198904285d324f66e
xinglei.li <xinglei.li@zhaopin.com.cn>
2020-08-28 19:41:27 +0800

    linkedlist

--- src/main/java/array/GenericArray.java
+++ src/main/java/array/GenericArray.java
@@ -1,5 +1,10 @@
 package array;
 
+/**
+ * 动态扩容的数组
+ *
+ * @param <T>
+ */
 public class GenericArray<T> {
     private T[] data;
     private int size;
@@ -15,41 +20,59 @@ public class GenericArray<T> {
         this(DEFAULT_CAPACITY);
     }
 
-    // 数组容量
+    /**
+     * 数组容量
+     */
     public int getCapacity() {
         return data.length;
     }
 
-    // 获取数组元素个数
+    /**
+     * 获取数组元素个数
+     */
     public int count() {
         return size;
     }
 
-    // 判读数组是否为空
+    /**
+     * 判读数组是否为空
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
-    // 修改 index 位置元素
+    /**
+     * 修改 index 位置元素
+     *
+     * @param index
+     * @param e
+     */
     public void set(int index, T e) {
         checkIndex(index);
         data[index] = e;
     }
 
-    // 获取 index 位置的元素
+    /**
+     * 获取 index 位置的元素
+     *
+     * @param index
+     * @return
+     */
     public T get(int index) {
         checkIndex(index);
         return data[index];
     }
 
-
-
-
+    /**
+     * 索引检查
+     *
+     * @param index
+     * @return
+     */
     private boolean checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new IllegalArgumentException("index must >= 0 and < size.");
         }
         return true;
     }
-
 }
--- src/main/java/linkedlist/SinglyLinkedList.java
+++ src/main/java/linkedlist/SinglyLinkedList.java
@@ -1,5 +1,30 @@
 package linkedlist;
 
+/**
+ * 1. 单链表反转
+ * 2. 链表中环的检测
+ * 3. 两个有序链表合并
+ * 4. 删除链表第 n 个节点
+ * 5. 求链表中间节点
+ */
 public class SinglyLinkedList {
+
+
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1, null);
+        Node node2 = new Node(1, null);
+        System.out.println(node1 == node2);
+    }
 
 }
